751f00338471d22d033d2139020ea6535a589459
Kshitija-saman <kshitija@samantechnosys.com>
2026-03-19 16:45:29 +0530

    Revert "Delete 2026_03_16_062410_create_brands_table.php"
    This reverts commit 137ed9bdf4e4e5398a44a9d78dc59c9db4e7dc0c.

---
+++ database/migrations/2026_03_16_062410_create_brands_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('workspace_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('brand_url')->unique();
+            $table->string('logo')->nullable();
+            $table->text('description')->nullable();
+            $table->string('status')->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('brands');
+    }
+};
